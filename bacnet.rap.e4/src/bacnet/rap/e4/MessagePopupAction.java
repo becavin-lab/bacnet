5279a21fb9aae97938e1735a7645c35911980fa6
becavin <becavin@ipmc.cnrs.fr>
2021-04-06 14:43:30 +0200

    Cleanout and add bacnet.rap.e4

---
+++ bacnet.rap.e4/src/bacnet/rap/e4/MessagePopupAction.java
@@ -0,0 +1,28 @@
+package bacnet.rap.e4;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * When run, this action will show a message dialog.
+ */
+public class MessagePopupAction extends Action {
+
+	private final IWorkbenchWindow window;
+
+	MessagePopupAction(String text, IWorkbenchWindow window) {
+		super(text);
+		this.window = window;
+		// The id is used to refer to the action in a menu or toolbar
+		setId(ICommandIds.CMD_OPEN_MESSAGE);
+		// Associate the action with a pre-defined command, to allow key bindings.
+		setActionDefinitionId(ICommandIds.CMD_OPEN_MESSAGE);
+		setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin("bacnet.rap.e4", "/icons/sample3.gif"));
+	}
+
+	public void run() {
+		MessageDialog.openInformation(window.getShell(), "Open", "Open Message Dialog!");
+	}
+}
